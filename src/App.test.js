3fff854eea2819a752997d8c1cf39b209a986818
Josep Servat <jservat@outlook.es>
2022-04-02 13:23:59 +0200

    Add a bunch of tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test("Renders Task Label", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("Task:");
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test("Renders priority Label", () => {
+  render(<App />);
+  const linkElement = screen.getByText("Priority:");
+  expect(linkElement).toBeInTheDocument();
+})
+
+test("renders table header - task column", () => {
+  render(<App />);
+  const linkElement = screen.getByText("Task");
+  expect(linkElement).toBeInTheDocument();
+})
+
+test("renders table header - priority column", () => {
+  render(<App />);
+  const linkElement = screen.getByText("Priority");
+  expect(linkElement).toBeInTheDocument();
+})
+
+test("renders table - first row task", () => {
+  render(<App />);
+  const linkElement = screen.getByText("Task 1");
+  expect(linkElement).toBeInTheDocument();
+})
+
+test("renders table - First Row Pritoriy", () => {
+  render(<App />);
+  const linkElement = screen.getByText("1");
+  expect(linkElement).toBeInTheDocument();
+})
---
+++ src/components/AddItem/AddItem.test.js
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react';
+import AddItem from './index';
+
+const empty = () => '';
+
+test("Renders Input Form", () => {
+  render(<AddItem addItem={empty} />);
+  const taskElement= screen.getByText("Task:");
+  expect(taskElement).toBeInTheDocument();
+
+  const priorityElement = screen.getByText("Priority:");
+  expect(priorityElement).toBeInTheDocument();
+})
